090b061dac2f81a80fe5ba52ab79295a0d155df4
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-11-06 22:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // your JS code here
-document.getElementById("sum_btn").addEventListener("click", function() {
-      const n1 = Number(document.getElementById("n1").value);
-      const n2 = Number(document.getElementById("n2").value);
-      const sum = n1 + n2;
-      document.getElementById("Sum").innerText = sum;
+let btn = document.getElementById("sum_btn");
+
+btn.addEventListener("click", function () {
+  let num1 = document.getElementById("n1").value;
+  let num2 = document.getElementById("n2").value;
+
+  if (num1 === "" || num2 === "") {
+    document.getElementById("sum").innerText = "";
+    return;
+  }
+
+  let sum = Number(num1) + Number(num2);
+  document.getElementById("sum").innerText = "Sum: " + sum;
+});
